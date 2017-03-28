dc8c7d00e33514833939074a4813346603f2da97
Valdegg <vae11@hi.is>
2017-03-28 19:31:55 +0000

    test mappa med metasearchclass test skeleton

---
+++ test/is/hi/tripPlanner/tripPlannerPackage/MetaSearchTest.java
@@ -0,0 +1,28 @@
+package is.hi.tripPlanner.tripPlannerPackage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Valdi on 28.3.2017.
+ */
+public class MetaSearchTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getDayTourInfo() throws Exception {
+
+    }
+
+}
