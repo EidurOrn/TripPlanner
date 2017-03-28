1962b212b638b1ed47059dffcf1a4c56a9bbd1a2
fridjon88 <fridjon88@gmail.com>
2017-03-28 16:37:03 +0000

    Moppu breytingar

--- src/is.hi.tripPlanner/dayTourPackage/mockObjects/DayTourFetching.java
+++
@@ -1,8 +0,0 @@
-package is.hi.tripPlanner.dayTourPackage.mockObjects;
-
-import is.hi.tripPlanner.dayTourPackage.SearchObject;
-import is.hi.tripPlanner.dayTourPackage.Trip;
-
-public interface DayTourFetching {
-    public Trip[] findResults(SearchObject s);
-}
