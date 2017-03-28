3c174fc5c601f5f6ed0c68d3c183048c07e7ad2d
Valdegg <vae11@hi.is>
2017-03-28 19:40:02 +0000

    testa ad koma testinu a github

---
+++ test/is/hi/tripPlanner/dayTourPackage/mockObjects/SearchModelTest.java
@@ -0,0 +1,23 @@
+package is.hi.tripPlanner.dayTourPackage.mockObjects;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Valdi on 28.3.2017.
+ * test klasi gerdur til ad profa github
+ */
+public class SearchModelTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
