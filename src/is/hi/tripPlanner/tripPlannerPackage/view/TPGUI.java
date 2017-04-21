0161927e30dd781dfbc3c915ae06cfa765ca5ad0
Fannsi <fthg2@hi.is>
2017-04-21 00:33:51 +0000

    view dot

--- src/is/hi/tripPlanner/tripPlannerPackage/view/TPGUI.java
+++ src/is/hi/tripPlanner/tripPlannerPackage/view/TPGUI.java
@@ -15,19 +15,42 @@ public class TPGUI {
     private JComboBox comboBox2;
     private JTable flightTable;
     private JList list1;
+    private JButton searchButton;
+    private JButton bookButton;
+    private JButton cancelButton;
+    private JComboBox hotelNameCB;
+    private JComboBox hotelLocationCB;
+    private JTable hotelTable;
+    private JButton hotelSearchBtn;
+    private JButton hotelCancelBtn;
+    private JButton hotelBookBtn;
+    private JTextField hotelAvailableFrom;
+    private JComboBox hotelNrNightCB;
+    private JComboBox comboBox3;
+    DefaultTableModel flightModel = new DefaultTableModel();
+    DefaultTableModel hotelModel = new DefaultTableModel();
 
     public TPGUI(){
-        DefaultTableModel model = new DefaultTableModel();
 
         flightTable.setAutoCreateRowSorter(true);
         flightTable.setFillsViewportHeight(true);
         flightTable.setPreferredScrollableViewportSize(new Dimension(550, 200));
-        model.addColumn("Location");
-        model.addColumn("Destination");
-        model.addColumn("Date");
-        model.addColumn("Price");
-        flightTable.setModel(model);
+        flightModel.addColumn("Location");
+        flightModel.addColumn("Destination");
+        flightModel.addColumn("Date");
+        flightModel.addColumn("Price");
+        flightTable.setModel(flightModel);
 
+        hotelTable.setAutoCreateRowSorter(true);
+        hotelTable.setFillsViewportHeight(true);
+        hotelTable.setPreferredScrollableViewportSize(new Dimension(550, 200));
+        hotelModel.addColumn("Name");
+        hotelModel.addColumn("Location");
+        hotelModel.addColumn("Price");
+        hotelModel.addColumn("Quality");
+        hotelModel.addColumn("Type");
+        hotelModel.addColumn("Theme");
+        hotelTable.setModel(hotelModel);
     }
 
     public static void main(String[] args) {
